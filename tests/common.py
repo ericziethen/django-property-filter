e8c6ea74266eee78dd5ec64db35d2e0b5f8ae896
eric <eric@home.com>
2020-05-24 14:54:24 +1000

    CLass COmparison

--- tests/common.py
+++ tests/common.py
@@ -1,19 +1,42 @@
-
-
-
+ 
 
 # https://stackoverflow.com/questions/34439/finding-what-methods-a-python-object-has
+def get_function_dic_for_class(obj):
+    # Some functions are unique for all classes
+    ignore_funcs = ['__init_subclass__', '__subclasshook__']
 
-# function comparison
-
-
-
-
-
-
+    return {name: getattr(obj, name) for name in dir(obj)
+            if callable(getattr(obj, name)) and name not in ignore_funcs}
 
 
+def class_functions_diff_dic(class1, class2):
+    diff_dic = {}
 
+    funcs1 = get_function_dic_for_class(class1)
+    funcs2 = get_function_dic_for_class(class2)
+    combined_names = set(list(funcs1.keys()) + list(funcs2.keys()))
 
+    in1_not2 = []
+    in2_not1 = []
+    different_sig = []
 
+    for name in combined_names:
+        if name in funcs1 and name in funcs2:
+            if funcs1[name] != funcs2[name]:
+                different_sig.append((name, funcs1[name], funcs2[name]))
+        else:
+            if name in funcs1:
+                in1_not2.append(name)
+            else:
+                in2_not1.append(name)
 
+    if in1_not2:
+        diff_dic[F'In {class1.__name__} not in {class2.__name__}'] = sorted(in1_not2)
+
+    if in2_not1:
+        diff_dic[F'In {class2.__name__} not in {class1.__name__}'] = sorted(in2_not1)
+
+    if different_sig:
+        diff_dic['Different Address'] = sorted(different_sig)
+
+    return diff_dic
--- tests/tests/test_common.py
+++ tests/tests/test_common.py
@@ -1,4 +1,50 @@
 
+from django.test import TestCase
+
+from tests.common import class_functions_diff_dic
+
+
+class ClassComparisonTests(TestCase):
+
+    def test_same_class_matching(self):
+        class Test():
+            def name(self):
+                return 'Test'
+
+        self.assertEqual(class_functions_diff_dic(Test, Test), {})
+
+    def test_sub_class_matching(self):
+        class Test():
+            def name(self):
+                return 'Test'
+
+        class SubTest(Test):
+            var = 'Me'
+
+        self.assertEqual(class_functions_diff_dic(Test, SubTest), {})
+
+    def test_different_class_not_matching(self):
+        class Test():
+            def name(self):
+                return 'Test'
+
+        class Test2():
+            def name(self):
+                return 'Test'
+
+        self.assertNotEqual(class_functions_diff_dic(Test, Test2), {})
+
+    def test_sub_class_not_matching(self):
+        class Test():
+            def name(self):
+                return 'Test'
+
+        class SubTest(Test):
+            def name(self):
+                return 'Test'
+
+        self.assertNotEqual(class_functions_diff_dic(Test, SubTest), {})
+
 
 # TODO - Implement Tests for Filterset Generators to ensure the function generator works
 
