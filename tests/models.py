5dec8af5e2a2c7cf8ea51bda1c84131eb0429ae4
eric <eric@home.com>
2020-05-29 16:33:19 +1000

    misc

--- tests/models.py
+++ tests/models.py
@@ -1,1 +1,8 @@
-"""Needed for Django Tests to Run."""
+"""Needed for Django Tests to Run.
+
+Test Models should be created in
+
+tests\django_test_proj\property_filter\models.py
+
+so that the Django Test Project can use them as well.
+"""
