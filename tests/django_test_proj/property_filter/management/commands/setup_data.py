a71a113376b94c1b0cb7d98968c20c4369403c76
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2020-06-03 09:19:24 +1000

    Add Text test data

--- tests/django_test_proj/property_filter/management/commands/setup_data.py
+++ tests/django_test_proj/property_filter/management/commands/setup_data.py
@@ -4,6 +4,7 @@ from django.core.management.base import BaseCommand
 from property_filter.models import (
     BooleanClass,
     NumberClass,
+    TextClass,
 )
 
 
@@ -13,10 +14,18 @@ class Command(BaseCommand):
         for num in range(21):
             NumberClass.objects.update_or_create(number=num)
 
-        # Boolean Filters
+        # BooleanClass Data
         BooleanClass.objects.update_or_create(id=1, is_true=True)
         BooleanClass.objects.update_or_create(id=2, is_true=False)
         BooleanClass.objects.update_or_create(id=3, )
         BooleanClass.objects.update_or_create(id=4, is_true=True)
         BooleanClass.objects.update_or_create(id=5, is_true=False)
         BooleanClass.objects.update_or_create(id=6, )
+
+        # TextClass Data
+        TextClass.objects.update_or_create(id=1, name='Tom')
+        TextClass.objects.update_or_create(id=2, name='Peter')
+        TextClass.objects.update_or_create(id=3, name='Ralph')
+        TextClass.objects.update_or_create(id=4, name='Tom')
+        TextClass.objects.update_or_create(id=5, name='TOM')
+        TextClass.objects.update_or_create(id=6, name='Tom')
