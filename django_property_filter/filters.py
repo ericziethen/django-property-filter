b34cb480032cbbd7f989b0411f38d2ede89fd8e8
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2020-06-26 14:13:23 +1000

    Fix linter issues

--- django_property_filter/filters.py
+++ django_property_filter/filters.py
@@ -131,10 +131,13 @@ class PropertyDurationFilter(PropertyBaseFilterMixin, DurationFilter):
     """Adding Property Support to DurationFilter."""
 
     supported_lookups = ['exact', 'gt', 'gte', 'lt', 'lte']
+
+
 class PropertyIsoDateTimeFromToRangeFilter(PropertyBaseFilterMixin, IsoDateTimeFromToRangeFilter):
     """Adding Property Support to IsoDateTimeFromToRangeFilter."""
 
     supported_lookups = ['range']
+
 
 class PropertyRangeFilter(PropertyBaseFilterMixin, RangeFilter):
     """Adding Property Support to RangeFilter."""
