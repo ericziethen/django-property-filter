4a1d46da8a874d315e7a07f85ee8d639ad19aa57
eric <48635802+ericziethen@users.noreply.github.com>
2020-07-25 19:41:10 +1000

    Display Database type on view

--- tests/django_test_proj/property_filter/templatetags/customtags.py
+++ tests/django_test_proj/property_filter/templatetags/customtags.py
@@ -1,7 +1,16 @@
 
 from django import template
+from django.db import connection
+
 
 register = template.Library()
+
+
 @register.filter
 def to_class_name(value):
     return value.__class__._meta.model.__name__
+
+
+@register.filter
+def db_provider(value):
+    return connection.vendor
