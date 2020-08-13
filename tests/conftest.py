bdbac29c56afb6681165847ae5fcb36c875e714d
Ziethen, EricSonke <48635802+ericziethen@users.noreply.github.com>
2020-08-13 15:14:46 +1000

    Fix travis marker

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,11 +8,11 @@ def pytest_collection_modifyitems(items):
     for item in items:
         skip_travis = item.get_closest_marker('skiptravis')
 
-        # Don't count in coverage to not be surprised late
+        # Don't count in coverage even when running to not be surprised later
         if skip_travis:
             item.add_marker(pytest.mark.no_cover)
 
-        # Skip if run on Travis
-        if is_travis_build():
-            print(F'Skip {item} because is Travis Build')
-            item.add_marker(pytest.mark.skip)
+            # Skip if run on Travis
+            if is_travis_build():
+                print(F'Skip {item} because is Travis Build')
+                item.add_marker(pytest.mark.skip)
