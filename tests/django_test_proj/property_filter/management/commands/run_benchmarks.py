db1524c106a1ac694667c76cae63af4a62158159
Ziethen, EricSonke <48635802+ericziethen@users.noreply.github.com>
2020-09-04 15:18:34 +1000

    progress

--- tests/django_test_proj/property_filter/management/commands/run_benchmarks.py
+++ tests/django_test_proj/property_filter/management/commands/run_benchmarks.py
@@ -133,8 +133,13 @@ class Command(BaseCommand):
     def create_filter_dics(self, filter_info_list):
         filter_dic = {}
         prop_filter_dic = {}
+        filter_names = []
+        prop_filter_names = []
 
         for filter_name, prop_filter_name, lookup_value in filter_info_list:
+            filter_names.append(filter_name)
+            prop_filter_names.append(prop_filter_name)
+
             filter_type = get_filtertype_from_filter_name(AllFiltersFilterSet(), filter_name)
             prop_filter_type = get_filtertype_from_filter_name(AllFiltersPropertyFilterSet(), prop_filter_name)
 
@@ -157,25 +162,26 @@ class Command(BaseCommand):
             else:
                 prop_filter_dic = {prop_filter_name: lookup_value}
 
-        return (filter_dic, prop_filter_dic)
+        return (filter_dic, filter_names, prop_filter_dic, prop_filter_names)
 
 
     def create_test_filtersets(self, filter_info_list):
-        filter_dic, prop_filter_dic = self.create_filter_dics(filter_info_list)
+        filter_dic, filter_names, prop_filter_dic, prop_filter_names = self.create_filter_dics(filter_info_list)
 
         # Create the Filtersets
         filter_fs = AllFiltersFilterSet(filter_dic, queryset=BenchmarkModel.objects.all())
         property_filter_fs = AllFiltersPropertyFilterSet(prop_filter_dic, queryset=BenchmarkModel.objects.all())
 
-        return (filter_fs, property_filter_fs)
+        return (filter_fs, filter_names, property_filter_fs, prop_filter_names)
 
 
     def run_all_filter_tests(self, base_data_dic):
+
         # Normal Filter
         for filter_name, prop_filter_name, lookup_value in SINGLE_FILTER_LOOKUP_LIST:
             result_list = []
 
-            filter_fs, property_filter_fs = self.create_test_filtersets([(filter_name, prop_filter_name, lookup_value)])
+            filter_fs, filter_names, property_filter_fs, prop_filter_names = self.create_test_filtersets([(filter_name, prop_filter_name, lookup_value)])
 
             # Run the Tests
             result_list.append(
@@ -184,20 +190,20 @@ class Command(BaseCommand):
 
             append_data_to_csv(self.csv_path, result_list)
 
-        '''
         # Multi Filter
-        for filter_name, prop_filter_name, lookup_value in MULTI_FILTER_LOOKUP_LIST:
+        for filter_info in MULTI_FILTER_LOOKUP_LIST:
             result_list = []
 
-            filter_fs, property_filter_fs = self.create_test_filtersets(filter_name, prop_filter_name, lookup_value)
+            filter_fs, filter_names, property_filter_fs, prop_filter_names = self.create_test_filtersets(filter_info)
 
             # Run the Tests
             result_list.append(
                 self._run_filter_comparison(
-                    filter_fs, property_filter_fs, base_data_dic.copy(), [filter_name], [prop_filter_name]))
+                    filter_fs, property_filter_fs, base_data_dic.copy(),
+                    filter_names, prop_filter_names))
 
             append_data_to_csv(self.csv_path, result_list)
-        '''
+
 
     def run_multi_filter_comparison(self, base_data_dic):
         filter_fs = MultiFilterFilterSet(
