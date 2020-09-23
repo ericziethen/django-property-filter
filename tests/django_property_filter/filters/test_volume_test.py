b13727d56f2d9c383733026f620c91d1214a68ea
Ziethen, EricSonke <48635802+ericziethen@users.noreply.github.com>
2020-09-23 14:47:54 +1000

    Full test

--- tests/django_property_filter/filters/test_volume_test.py
+++ tests/django_property_filter/filters/test_volume_test.py
@@ -58,7 +58,7 @@ UUID_RANGE = [
 class LargeVolumeTests(TestCase):
 
     def setUp(self):
-        db_entry_count = 350
+        db_entry_count = 300000
 
         # Generate Random Data
         bulk_list = []
