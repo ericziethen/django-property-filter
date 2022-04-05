7a9f8bfbd0ad3da1c89dc8b3af0c040af508e79d
eric <48635802+ericziethen@users.noreply.github.com>
2022-04-05 21:23:04 +1000

    Add tests and documentation for order of property filters

--- tests/django_property_filter/test_filtersets.py
+++ tests/django_property_filter/test_filtersets.py
@@ -1,6 +1,8 @@
 
 import pytest
 
+from django.test import TestCase
+
 from django_property_filter import (
     PropertyFilterSet,
     PropertyChoiceFilter,
@@ -10,6 +12,7 @@ from django_property_filter import (
 from django_filters import FilterSet
 
 from property_filter.models import (
+    DoubleIntModel,
     Product,
     RangeFilterModel
 )
@@ -165,3 +168,118 @@ def test_disallowed_implicid_filter_created():
     with pytest.raises(ValueError):
         Fs({'prop_name': '5'}, queryset=Product.objects.all())
 
+
+
+
+
+
+from unittest import mock
+from django_property_filter import compare_by_lookup_expression
+class FilterOrderTests(TestCase):
+
+    def setUp(self):
+        DoubleIntModel.objects.create(id=0, age=30, number=1)
+        DoubleIntModel.objects.create(id=1, age=30, number=1)
+        DoubleIntModel.objects.create(id=2, age=20, number=1)
+        DoubleIntModel.objects.create(id=3, age=20, number=1)
+        DoubleIntModel.objects.create(id=4, age=20, number=1)
+        DoubleIntModel.objects.create(id=5, age=20, number=1)
+        DoubleIntModel.objects.create(id=6, age=20, number=1)
+        DoubleIntModel.objects.create(id=7, age=20, number=1)
+        DoubleIntModel.objects.create(id=8, age=20, number=2)
+        DoubleIntModel.objects.create(id=9, age=20, number=2)
+
+    def test_filter_order_explicit_declaration(self):
+        class MyPropertyFilterset(PropertyFilterSet):
+            prop_number = PropertyNumberFilter(field_name='prop_number', lookup_expr='exact')
+            prop_age = PropertyNumberFilter(field_name='prop_age', lookup_expr='exact')
+
+            class Meta:
+                model = DoubleIntModel
+                exclude = ['age', 'number']
+                fields = ['prop_age', 'prop_number']  # Order of filtering
+
+        fs = MyPropertyFilterset({'prop_age': 20, 'prop_number': 2}, queryset=DoubleIntModel.objects.all())
+
+        with mock.patch('django_property_filter.filters.compare_by_lookup_expression', side_effect=compare_by_lookup_expression) as mock_func:
+            query_set = fs.qs
+
+            # 1.) Filter for 'prop_age:20', -> Going 10 times into compare_by_lookup_expression, 8 results
+            # 2.) Filter for 'prop_number:2',    -> Going 8 times into compare_by_lookup_expression, 2 results
+            #        -> 2 results and 18 x function execution
+
+            assert query_set.count() == 2
+            assert mock_func.call_count == 18
+
+    def test_filter_order_implicit_declaration(self):
+        class MyPropertyFilterset(PropertyFilterSet):
+
+            class Meta:
+                model = DoubleIntModel
+                exclude = ['age', 'number']
+                # fields = ['prop_age', 'prop_number']  # Order of filtering
+                property_fields = [
+                    ('prop_age', PropertyNumberFilter, ['exact']),
+                    ('prop_number', PropertyNumberFilter, ['exact']),
+                    ]
+
+
+        fs = MyPropertyFilterset({'prop_age__exact': 20, 'prop_number__exact': 2}, queryset=DoubleIntModel.objects.all())
+
+        with mock.patch('django_property_filter.filters.compare_by_lookup_expression', side_effect=compare_by_lookup_expression) as mock_func:
+            query_set = fs.qs
+
+            # 1.) Filter for 'prop_age:20', -> Going 10 times into compare_by_lookup_expression, 8 results
+            # 2.) Filter for 'prop_number:2',    -> Going 8 times into compare_by_lookup_expression, 2 results
+            #        -> 2 results and 18 x function execution
+
+            assert query_set.count() == 2
+            assert mock_func.call_count == 18
+
+    def test_reverse_filter_order_explicit_declaration(self):
+
+        class MyPropertyFilterset(PropertyFilterSet):
+            prop_number = PropertyNumberFilter(field_name='prop_number', lookup_expr='exact')
+            prop_age = PropertyNumberFilter(field_name='prop_age', lookup_expr='exact')
+
+            class Meta:
+                model = DoubleIntModel
+                exclude = ['age', 'number']
+                fields = ['prop_number', 'prop_age']  # Order of filtering
+
+        fs = MyPropertyFilterset({'prop_age': 20, 'prop_number': 2}, queryset=DoubleIntModel.objects.all())
+
+        with mock.patch('django_property_filter.filters.compare_by_lookup_expression', side_effect=compare_by_lookup_expression) as mock_func:
+            query_set = fs.qs
+
+            # 1.) Filter for 'prop_number:2',    -> Going 10 times into compare_by_lookup_expression, 2 results
+            # 2.) Filter for 'prop_age:20', -> Going 2 times into compare_by_lookup_expression, 2 results
+            #    -> 2 results and 12 x function execution
+
+            assert query_set.count() == 2
+            assert mock_func.call_count == 12
+
+    def test_filter_reverse_order_implicit_declaration(self):
+        class MyPropertyFilterset(PropertyFilterSet):
+
+            class Meta:
+                model = DoubleIntModel
+                exclude = ['age', 'number']
+                # fields = ['prop_age', 'prop_number']  # Order of filtering
+                property_fields = [
+                    ('prop_number', PropertyNumberFilter, ['exact']),
+                    ('prop_age', PropertyNumberFilter, ['exact']),
+                    ]
+
+
+        fs = MyPropertyFilterset({'prop_age__exact': 20, 'prop_number__exact': 2}, queryset=DoubleIntModel.objects.all())
+
+        with mock.patch('django_property_filter.filters.compare_by_lookup_expression', side_effect=compare_by_lookup_expression) as mock_func:
+            query_set = fs.qs
+
+            # 1.) Filter for 'prop_age:20', -> Going 10 times into compare_by_lookup_expression, 8 results
+            # 2.) Filter for 'prop_number:2',    -> Going 8 times into compare_by_lookup_expression, 2 results
+            #        -> 2 results and 18 x function execution
+
+            assert query_set.count() == 2
+            assert mock_func.call_count == 12
