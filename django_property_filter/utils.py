acfa1830286b21c39b3a210c803a56ddc037e068
eric <48635802+ericziethen@users.noreply.github.com>
2023-12-24 17:40:16 +1100

    Test passed for Single Related Model

--- django_property_filter/utils.py
+++ django_property_filter/utils.py
@@ -167,7 +167,11 @@ def get_value_for_db_field(obj, field_str):
     """Lookup a model field or property."""
     def get_attr_val_recursive(obj, sub_list):
         if len(sub_list) == 1:
-            return getattr(obj, sub_list[0])
+            try:
+                found_value = getattr(obj, sub_list[0])
+            except (AttributeError):
+                found_value = None
+            return found_value
 
         new_object = getattr(obj, sub_list[0])
         return get_attr_val_recursive(new_object, sub_list[1:])
