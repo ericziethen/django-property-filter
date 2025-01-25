f3a1d643430f83bcee8bb11902a3367bea71ab4e
eric <48635802+ericziethen@users.noreply.github.com>
2025-01-25 16:57:07 +1100

    Fix release check

--- dev/Packaging/check_github_actions_release.sh
+++ dev/Packaging/check_github_actions_release.sh
@@ -6,21 +6,21 @@ echo '##### Calling: '`basename "$0"` '('$0')'
 echo "Github Actions Tag '$GITHUB_REF'"
 
 # Ensure we can identify our own version
-SETUP_CFG_VERSION="$(sed -nr 's/^version\s*=\s*([0-9]+\.[0-9]+\.[0-9]+)\s*$/\1/p' setup.cfg)"
+REPO_VERSION="$(sed -nr 's/^version\s*=\s*\"([0-9]+\.[0-9]+\.[0-9]+)\"\s*$/\1/p' pyproject.toml)"
 
-if [ -z "$SETUP_CFG_VERSION" ]
+if [ -z "$REPO_VERSION" ]
 then
-    echo "Failed to identify the Setup.cfg Version"
+    echo "Failed to identify the pyproject.toml Version"
     exit 1
 else
-    echo "Repository Version identified: '$SETUP_CFG_VERSION'"
+    echo "Repository Version identified: '$REPO_VERSION'"
 fi
 
 # Check the Github Actions Tag matches our Version number
-echo "Trying to Match '$SETUP_CFG_VERSION' to '$GITHUB_REF'"
-if [ "refs/tags/$SETUP_CFG_VERSION" != "$GITHUB_REF" ]
+echo "Trying to Match '$REPO_VERSION' to '$GITHUB_REF'"
+if [ "refs/tags/$REPO_VERSION" != "$GITHUB_REF" ]
 then
-    echo "Failed to match Repository Version '$SETUP_CFG_VERSION' to Github Actions Tag '$GITHUB_REF'"
+    echo "Failed to match Repository Version '$REPO_VERSION' to Github Actions Tag '$GITHUB_REF'"
     exit 1
 else
     echo "Repository Version matches Github Actions Tag"
